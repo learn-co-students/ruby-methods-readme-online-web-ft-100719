9dc646794ca1d0abd671b92ab29155d29366af8e
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-07 18:50:58 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,14 @@
 # Use this space to code along with the readme
-
+$ greeting.rb 
 phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+end 
